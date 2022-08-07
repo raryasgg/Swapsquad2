4759237a59a4deb8c0a420dd9f3d4575c574f580
Kumar <abhay.kumar2@cgi.com>
2022-08-07 21:22:57 +0530

    updated product detail

--- product-webapp/webapp/src/app/component/productdetails/material1/material1.module.ts
+++ product-webapp/webapp/src/app/component/productdetails/material1/material1.module.ts
@@ -5,14 +5,20 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatChipsModule} from '@angular/material/chips';
+
 const MaterialComponents=[
   MatButtonModule,
   MatCardModule,
   MatGridListModule,
   MatIconModule,
   MatButtonToggleModule,
-  MatCheckboxModule
-  
+  MatCheckboxModule,
+  FlexLayoutModule,
+  MatPaginatorModule,
+  MatChipsModule
 
 ]
 
