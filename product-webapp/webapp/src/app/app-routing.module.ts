7ea3eead16ddf7881722019c26eab8dba8c8e558
raryasgg <raryasgg@gmail.com>
2022-08-08 12:57:07 +0530

    error resolved

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -1,19 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
+import { ProductdetailsComponent } from './component/productdetails/productdetails.component';
+import { RegisterProductComponent } from './component/register-product/register-product.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
 
 const routes: Routes = [
-  {path:'registerUser',component:RegisterUserComponent}
-=======
-import { ProductdetailsComponent } from './component/productdetails/productdetails.component';
-import { RegisterProductComponent } from './component/register-product/register-product.component';
-
-
-const routes: Routes = [
+  {path:'registerUser',component:RegisterUserComponent},
   {path : 'registerProduct', component : RegisterProductComponent},
-  {path : 'productDetail', component : ProductdetailsComponent},
->>>>>>> 1187ab91b12d8342c954442f8a0883132bba7554
+  {path : 'productDetail', component : ProductdetailsComponent}
+
 ];
 
 @NgModule({
--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -1,59 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { RegisterUserComponent } from './register-user/register-user.component';
-import {HttpClientModule} from '@angular/common/http';
-import {MatButtonModule} from '@angular/material/button';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-@NgModule({
-  declarations: [
-    AppComponent,
-    RegisterUserComponent
-=======
 import { RegisterProductComponent } from './component/register-product/register-product.component';
 import { MaterialModule } from './component/register-product/module/material/material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Material1Module } from './component/productdetails/material1/material1.module';
 import { ProductdetailsComponent } from './component/productdetails/productdetails.component';
-
+import { RegisterUserComponent } from './register-user/register-user.component';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterProductComponent,
-    ProductdetailsComponent
->>>>>>> 1187ab91b12d8342c954442f8a0883132bba7554
+    ProductdetailsComponent,
+    RegisterUserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
-    FormsModule,
-    HttpClientModule,
-    MatButtonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatRadioModule,
-    MatGridListModule,
-    MatToolbarModule
-=======
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    Material1Module
+    Material1Module,
+    HttpClientModule
 
->>>>>>> 1187ab91b12d8342c954442f8a0883132bba7554
   ],
   providers: [],
   bootstrap: [AppComponent]
