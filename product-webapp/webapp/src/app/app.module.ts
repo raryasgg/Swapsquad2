aa6f8fd055f6493df2a8b2cf89acdd86d42c131a
Chitranshu Singh <chitranshu1003@gmail.com>
2022-08-15 12:20:32 +0530

    done

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -70,17 +70,17 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
     MatButtonToggleModule,
     MatProgressBarModule,
     FlexLayoutModule,
-<<<<<<< HEAD
-    
-=======
 
->>>>>>> 687952eb2f2d7694deb9653c7ec2945da63a1ad2
+    
+
+
+
   ],
   providers: [ProductDetailsService],
   bootstrap: [AppComponent],
 })
-<<<<<<< HEAD
+
 export class AppModule {}
-=======
-export class AppModule { }
->>>>>>> 687952eb2f2d7694deb9653c7ec2945da63a1ad2
+
+
+
