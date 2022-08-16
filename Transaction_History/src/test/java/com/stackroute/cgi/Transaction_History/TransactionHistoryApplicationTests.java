632d8ebd23221e3c696ef5fe42eede675643e445
Gaurav Srivastava <gsrivastava426@gmail.com>
2022-08-16 11:50:05 +0530

    done

---
+++ Transaction_History/src/test/java/com/stackroute/cgi/Transaction_History/TransactionHistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stackroute.cgi.Transaction_History;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionHistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
