ad76d65324b527b78c0dacb24b8402b4b26b425f
AVANI <avaniammu25@gmail.com>
2022-08-16 11:06:29 +0530

    updated angular version

--- product-webapp/webapp/src/app/app.module.ts
+++ product-webapp/webapp/src/app/app.module.ts
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterProductComponent } from './component/register-product/register-product.component';
-import { MaterialModule } from './component/register-product/module/material/material.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Material1Module } from './component/productdetails/material1/material1.module';
-import { ProductdetailsComponent } from './component/productdetails/productdetails.component';
-import { RegisterUserComponent } from './register-user/register-user.component';
-import {HttpClientModule} from '@angular/common/http';
-
-import { RecommendationServiceComponent } from './recommendation-service/recommendation-service.component';
-import {MatPaginatorModule} from '@angular/material/paginator';
-
-
-import { Material3Module } from './register-user/material3/material3.module';
-
-import { ProductDetailsService } from './component/productdetails/product-details.service';
-import { SweetAlertGrow } from 'sweetalert2';
-import { LoginComponent } from 'src/login/login.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
-=======
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
@@ -58,8 +32,6 @@ import { MatProgressBarModule } from "@angular/material/progress-bar";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { UpdateDetailsComponent } from './update-details/update-details.component';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { TranscationsComponent } from './transcations/transcations.component';
->>>>>>> 9a1b1dfbcf3d12051dca9678fd800e7f4da65cf9
 
 @NgModule({
   declarations: [
@@ -67,16 +39,9 @@ import { TranscationsComponent } from './transcations/transcations.component';
     RegisterProductComponent,
     ProductdetailsComponent,
     RegisterUserComponent,
-<<<<<<< HEAD
-     RecommendationServiceComponent,
-     LoginComponent,
-     
-=======
     RecommendationServiceComponent,
     UpdateDetailsComponent,
-    TranscationsComponent,
 
->>>>>>> 9a1b1dfbcf3d12051dca9678fd800e7f4da65cf9
   ],
 
   imports: [
@@ -85,12 +50,6 @@ import { TranscationsComponent } from './transcations/transcations.component';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-<<<<<<< HEAD
-    MatButtonModule,
-    MatInputModule,
-    Material1Module,
-=======
->>>>>>> 9a1b1dfbcf3d12051dca9678fd800e7f4da65cf9
     HttpClientModule,
     MatAutocompleteModule,
     MatChipsModule,
@@ -115,14 +74,5 @@ import { TranscationsComponent } from './transcations/transcations.component';
   providers: [ProductDetailsService],
   bootstrap: [AppComponent],
 })
-<<<<<<< HEAD
-export class AppModule { }
-
-
-
-
-
-=======
 
 export class AppModule {}
->>>>>>> 9a1b1dfbcf3d12051dca9678fd800e7f4da65cf9
