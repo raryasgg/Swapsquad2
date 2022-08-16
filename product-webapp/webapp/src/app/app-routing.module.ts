d18d53106d3d7af48740101addc1f1cc86ec0248
Kumar <abhay.kumar2@cgi.com>
2022-08-16 14:52:09 +0530

    conflict error resolved

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -6,18 +6,6 @@ import { ProductdetailsComponent } from './component/productdetails/productdetai
 import { RegisterProductComponent } from './component/register-product/register-product.component';
 import { RecommendationServiceComponent } from './recommendation-service/recommendation-service.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
-<<<<<<< HEAD
-import { LoginComponent } from 'src/login/login.component';
-
-const routes: Routes = [
-
-  {path : 'registerUser', component : RegisterUserComponent},
-  {path : 'registerProduct', component : RegisterProductComponent},
-  {path : 'productDetail', component : ProductdetailsComponent},
-  {path:'recommendation-service', component:RecommendationServiceComponent},
-  // {path :'login',component:LoginComponent},
-
-=======
 import { UpdateDetailsComponent } from './update-details/update-details.component';
 import { TranscationsComponent } from './transcations/transcations.component';
 const routes: Routes = [
@@ -29,7 +17,7 @@ const routes: Routes = [
   { path: 'updateDetails', component: UpdateDetailsComponent },
   { path: 'home', component: AppComponent },
   {path:'transcations', component:TranscationsComponent}
->>>>>>> 9a1b1dfbcf3d12051dca9678fd800e7f4da65cf9
+
 
 
 ];
