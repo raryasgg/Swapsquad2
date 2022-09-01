fee5a11369a3e71692dfa7123635e91de485860a
abhaykumar789 <101271007+abhaykumar789@users.noreply.github.com>
2022-09-01 17:09:59 +0530

    update

--- product-webapp/webapp/src/app/app-routing.module.ts
+++ product-webapp/webapp/src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ children:[
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
