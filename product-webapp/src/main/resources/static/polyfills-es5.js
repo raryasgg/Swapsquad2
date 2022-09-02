9d83b5eb62782903b6e7d2b8719ee1e5046f44b9
abhaykumar789 <101271007+abhaykumar789@users.noreply.github.com>
2022-09-02 14:40:58 +0530

    transaction by coin update

--- product-webapp/src/main/resources/static/polyfills-es5.js
+++ product-webapp/src/main/resources/static/polyfills-es5.js
@@ -466,7 +466,7 @@
     /***/
     function _(module, exports, __webpack_require__) {
       __webpack_require__(
-      /*! /home/chaitali/Documents/swapsquad/product-webapp/webapp/node_modules/@angular-devkit/build-angular/src/webpack/es5-polyfills.js */
+      /*! D:\CGI\Project\SwapSquad_1Sep\swapsquad\product-webapp\webapp\node_modules\@angular-devkit\build-angular\src\webpack\es5-polyfills.js */
       "voQr");
 
       __webpack_require__(
@@ -474,7 +474,7 @@
       "R0gw");
 
       module.exports = __webpack_require__(
-      /*! /home/chaitali/Documents/swapsquad/product-webapp/webapp/src/polyfills.ts */
+      /*! D:\CGI\Project\SwapSquad_1Sep\swapsquad\product-webapp\webapp\src\polyfills.ts */
       "hN/g");
       /***/
     },
